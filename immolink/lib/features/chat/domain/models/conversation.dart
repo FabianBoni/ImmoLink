99dca6f548caee69788884a3d396fd2585e00cdd
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2025-02-17 20:29:44 +0100

    update
    first version of the working bot, adjusted and optimized main, routes, and the services

--- immolink/lib/features/chat/domain/models/conversation.dart
+++ immolink/lib/features/chat/domain/models/conversation.dart
@@ -1,20 +1,29 @@
 class Conversation {
   final String id;
-  final String otherUserName;
-  final String? lastMessage;
+  final String propertyId;
+  final String landlordId;
+  final String tenantId;
+  final String propertyAddress;
+  final String lastMessage;
   final DateTime lastMessageTime;
 
   Conversation({
     required this.id,
-    required this.otherUserName,
-    this.lastMessage,
+    required this.propertyId,
+    required this.landlordId,
+    required this.tenantId,
+    required this.propertyAddress,
+    required this.lastMessage,
     required this.lastMessageTime,
   });
 
   factory Conversation.fromMap(Map<String, dynamic> map) {
     return Conversation(
-      id: map['id'],
-      otherUserName: map['otherUserName'],
+      id: map['_id'],
+      propertyId: map['propertyId'],
+      landlordId: map['landlordId'],
+      tenantId: map['tenantId'],
+      propertyAddress: map['propertyAddress'],
       lastMessage: map['lastMessage'],
       lastMessageTime: DateTime.parse(map['lastMessageTime']),
     );
